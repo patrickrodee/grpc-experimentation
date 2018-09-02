553696d41326b8f5eba9f91c8b345489ff046ccc
Patrick RoDee <patrick.d.rodee@gmail.com>
2018-09-02 01:51:20 +0000

    fixed formatting

--- src/client/client.go
+++ src/client/client.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"log"
-	"os"
-	"time"
+  "log"
+  "os"
+  "time"
 
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	pb "github.com/patrickrodee/grpc-experimentation/protos/helloworld"
+  "golang.org/x/net/context"
+  "google.golang.org/grpc"
+  pb "github.com/patrickrodee/grpc-experimentation/protos/helloworld"
 )
 
 const (
-	address     = "localhost:50051"
-	defaultName = "world"
+  address     = "localhost:50051"
+  defaultName = "world"
 )
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
+  // Set up a connection to the server.
+  conn, err := grpc.Dial(address, grpc.WithInsecure())
+  if err != nil {
+    log.Fatalf("did not connect: %v", err)
+  }
+  defer conn.Close()
+  c := pb.NewGreeterClient(conn)
 
-	// Contact the server and print out its response.
-	name := defaultName
-	if len(os.Args) > 1 {
-		name = os.Args[1]
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	log.Printf("Greeting: %s", r.Reply)
+  // Contact the server and print out its response.
+  name := defaultName
+  if len(os.Args) > 1 {
+    name = os.Args[1]
+  }
+  ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+  defer cancel()
+  r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+  if err != nil {
+    log.Fatalf("could not greet: %v", err)
+  }
+  log.Printf("Greeting: %s", r.Reply)
 }
--- src/server/server.go
+++ src/server/server.go
@@ -2,7 +2,7 @@ package main
 
 import (
   "log"
-	"net"
+  "net"
   
   "golang.org/x/net/context"
   "google.golang.org/grpc"
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	port = ":50051"
+  port = ":50051"
 )
 
 // server is used to implement helloworld.GreeterServer.
@@ -20,20 +20,20 @@ type server struct{}
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
   log.Printf("Message received: %s\n", in.Name)
-	return &pb.HelloResponse{Reply: "Hello " + in.Name}, nil
+  return &pb.HelloResponse{Reply: "Hello " + in.Name}, nil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
+  lis, err := net.Listen("tcp", port)
+  if err != nil {
+    log.Fatalf("failed to listen: %v", err)
+  }
   log.Print("Server started")
-	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+  s := grpc.NewServer()
+  pb.RegisterGreeterServer(s, &server{})
 	// Register reflection service on gRPC server.
-	reflection.Register(s)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+  reflection.Register(s)
+  if err := s.Serve(lis); err != nil {
+    log.Fatalf("failed to serve: %v", err)
+  }
 }
